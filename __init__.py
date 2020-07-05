7cdb49d6ba530cf4840bb2e5dab3ab645525035d
Govindan Palani <67826158+Govindan-2412@users.noreply.github.com>
2020-07-05 09:03:19 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
-"""Subpackage containing all of pip's command line interface related code
-"""
+"""CacheControl import Interface.
 
-# This file intentionally does not import submodules
+Make it easy to import from cachecontrol without long namespaces.
+"""
+__author__ = "Eric Larson"
+__email__ = "eric@ionrock.org"
+__version__ = "0.12.6"
+
+from .wrapper import CacheControl
+from .adapter import CacheControlAdapter
+from .controller import CacheController
