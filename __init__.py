b374335e162fc75be6a520a654bbd32f016525d4
Govindan Palani <67826158+Govindan-2412@users.noreply.github.com>
2020-07-05 09:09:17 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,11 +1,3 @@
-"""CacheControl import Interface.
+from .core import contents, where
 
-Make it easy to import from cachecontrol without long namespaces.
-"""
-__author__ = "Eric Larson"
-__email__ = "eric@ionrock.org"
-__version__ = "0.12.6"
-
-from .wrapper import CacheControl
-from .adapter import CacheControlAdapter
-from .controller import CacheController
+__version__ = "2020.04.05.1"
